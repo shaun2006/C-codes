11167283b8174fa524598cc9c1e6bdc329ff5a6c
Shaun2006 <116490490+shaun2006@users.noreply.github.com>
2025-05-27 16:49:31 +0530

    basic mathematics
    i have done some basic mathematical operations like addition, subtraction, multiplication, division

---
+++ basic_maths.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main ()
+{
+	int first_number = 10;
+	int second_number = 60;
+
+	// addition
+	printf("%d\n", first_number+second_number);
+	
+	// Positive subtraction
+	printf("%d\n", second_number - first_number);
+
+	// Negative subtraction
+	printf("%d\n", first_number - second_number);
+
+	// Multiplication
+	printf("%d\n", first_number * second_number);
+
+	// division
+	printf("%d\n", second_number / first_number );
+
+	return 0;
+}
