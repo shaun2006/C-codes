3719ba95764290dfc19cce21b68384125e25a721
Shaun2006 <116490490+shaun2006@users.noreply.github.com>
2025-06-02 22:13:40 +0530

    Update loops.c
    adding while loop

--- loops.c
+++ loops.c
@@ -3,6 +3,12 @@
 int main() {
     for( int a = 0; a <= 10; a++){
       printf("Hello world\n");
-  }
+    }
+    int i = 0;
+    while (i < 5) {
+        printf("%d\n", i);
+        i++;
+    }
     return 0;
 }
+
