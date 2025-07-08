489da912db4b1161664716c339f562a6cab31292
Shaun2006 <116490490+shaun2006@users.noreply.github.com>
2025-07-09 00:10:56 +0530

    win-msg-box
    simple message box in c using windows.h

---
+++ win-msg-box.c
@@ -0,0 +1,6 @@
+#include <windows.h>
+
+int main() {
+  MessageBox(NULL, "hello world", "Box", MB_OK);    // ref https://learn.microsoft.com/en-us/windows/win32/api/winuser/nf-winuser-messagebox
+  return 0;
+}
